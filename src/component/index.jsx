00f9aef41a12f96559bbe085aa7a68f0318d4d5d
Tanzim313 <tanzimhasanrizbi@gmail.com>
2026-01-22 11:34:07 +0600

    tanstack query added and login using daisy

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -2,12 +2,32 @@ import React from "react";
 
 const Index=()=>{
 
+    
+
    return(
-    <div>
-       
+
+<div className="flex justify-center mt-40">
+
+<form className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+    <fieldset className="fieldset">
+    <label className="label">Email</label>
+    <input type="email" className="input validator" placeholder="Email" required />
+    <p className="validator-hint hidden">Required</p>
+    </fieldset>
+
+    <label className="fieldset">
+    <span className="label">Password</span>
+    <input type="password" className="input validator" placeholder="Password" required />
+    <span className="validator-hint hidden">Required</span>
+    </label>
+
+    <button className="btn btn-neutral mt-4" type="submit">Login</button>
+    
+</form>
+
 
     </div>
-   )
+)
     
 
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Index from "./component";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
 
@@ -16,8 +18,13 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+
+    <QueryClientProvider client={queryClient}>
+      {/* The rest of your application */}
+        <RouterProvider router={router} />
     
-      <RouterProvider router={router} />
+    </QueryClientProvider>
+    
     
   </StrictMode>
 );
