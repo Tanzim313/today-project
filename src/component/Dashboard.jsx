ac99ebc996c756f68590989f451ccb3bcaed6c52
Tanzim313 <tanzimhasanrizbi@gmail.com>
2026-01-22 12:15:30 +0600

    todo added add teask desing

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -8,7 +8,16 @@ const Dashboard=()=>{
 
             <h1>To Do Dashboard</h1>
 
-            
+
+            <div>
+                <h1>To do Task Add</h1>
+                <input  type="text" placeholder="Type here" className="input input-ghost border-2 border-amber-500 " />
+
+                <button className="btn btn-neutral">Add</button>
+            </div>
+
+    
+
 
         </div>
     )
